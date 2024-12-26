4587a62508046eb958915c3c554efb1563907d0d
AetherMind-Ai <clashinkofsr@gmail.com>
2024-12-26 23:32:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='mindbotai',
-    version='0.1.0',  # Change as needed
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'google-generativeai',  # Add any other dependencies
+        'google-generativeai',
+         'python-dotenv',
     ],
 )
