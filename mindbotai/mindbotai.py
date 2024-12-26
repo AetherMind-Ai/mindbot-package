0384e1dde59ee15ed5d06cf46c46d7543313cab5
AetherMind-Ai <clashinkofsr@gmail.com>
2024-12-26 23:28:50 +0200

    Update mindbotai.py

--- mindbotai/mindbotai.py
+++ mindbotai/mindbotai.py
@@ -1,15 +1,18 @@
 # mindbotai.py
 import google.generativeai as mindai
 import os
+from dotenv import load_dotenv
 
-api_key = os.getenv("MIND_API_KEY") # Get the api key from os.env
+load_dotenv()  # Load variables from .env file
+
+api_key = os.getenv("MIND_API_KEY")
 if not api_key:
-    raise ValueError("MIND_API_KEY environment variable not set")
+     raise ValueError("MIND_API_KEY environment variable not set")
 
 mindai.configure(api_key=api_key)
 mindbot1_3= "gemini-1.5-flash"
 
 def generate_content(prompt):
-    model = mindai.GenerativeModel(mindbot1_3)
-    response = model.generate_content(prompt)
-    return response.text
+     model = mindai.GenerativeModel(mindbot1_3)
+     response = model.generate_content(prompt)
+     return response.text
